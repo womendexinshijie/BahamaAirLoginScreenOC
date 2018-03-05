46bb0f7adcd0207da8d912a628663b6fbaa6047a
womendexinshijie <womendexinshijie@sina.com>
2018-03-05 09:30:20 +0800

    iOS.Animations.by.Tutorials.v2.0汉化
    第一节第一章

--- BahamaAirLoginScreenOC/ViewController.m
+++ BahamaAirLoginScreenOC/ViewController.m
@@ -9,6 +9,16 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UILabel *heading;
+@property (weak, nonatomic) IBOutlet UITextField *userName;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+
+@property (weak, nonatomic) IBOutlet UIImageView *cloud1;
+@property (weak, nonatomic) IBOutlet UIImageView *cloud2;
+@property (weak, nonatomic) IBOutlet UIImageView *cloud3;
+@property (weak, nonatomic) IBOutlet UIImageView *cloud4;
+
 
 @end
 
@@ -17,8 +27,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    _heading.center = CGPointMake(_heading.center.x-self.view.frame.size.width, _heading.center.y);
+    _userName.center = CGPointMake(_userName.center.x-self.view.frame.size.width, _userName.center.y);
+    _password.center = CGPointMake(_password.center.x-self.view.frame.size.width, _password.center.y);
+    
+    //////////////////////////////////////////////第一章
+//    self.loginButton.center = CGPointMake(self.loginButton.center.x, self.loginButton.center.y + 30.0);
+//    self.loginButton.alpha = 0.0;
+
+   
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [UIView animateWithDuration:0.5 animations:^{
+        _heading.center = CGPointMake(_heading.center.x+self.view.frame.size.width, _heading.center.y);
+    }];
+    
+    [UIView animateWithDuration:0.5 delay:0.3 options:nil animations:^{
+        _userName.center = CGPointMake(_userName.center.x+self.view.frame.size.width, _userName.center.y);
+    } completion:nil];
+    [UIView animateWithDuration:0.5 delay:0.4 options:nil animations:^{
+        _password.center = CGPointMake(_password.center.x+self.view.frame.size.width, _password.center.y);
+    } completion:nil];
+//    密码控件反复
+//    [UIView animateWithDuration:0.5 delay:0.4 options:UIViewAnimationOptionRepeat animations:^{
+//        _password.center = CGPointMake(_password.center.x+self.view.frame.size.width, _password.center.y);
+//    } completion:nil];
+//    密码控件往返循环
+//    [UIView animateWithDuration:0.5 delay:0.4 options:UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse animations:^{
+//        _password.center = CGPointMake(_password.center.x+self.view.frame.size.width, _password.center.y);
+//    } completion:nil];
+//    密码控件往返循环 减速结束
+//    [UIView animateWithDuration:0.5 delay:0.4 options:UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse|UIViewAnimationOptionCurveEaseOut animations:^{
+//        _password.center = CGPointMake(_password.center.x+self.view.frame.size.width, _password.center.y);
+//    } completion:nil];
+//    密码控件往返循环 加速开始
+//    [UIView animateWithDuration:0.5 delay:0.4 options:UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse|UIViewAnimationOptionCurveEaseIn animations:^{
+//        _password.center = CGPointMake(_password.center.x+self.view.frame.size.width, _password.center.y);
+//    } completion:nil];
+//    密码控件往返循环 加速开始  减速结束
+//    [UIView animateWithDuration:0.5 delay:0.4 options:UIViewAnimationOptionRepeat|UIViewAnimationOptionAutoreverse|UIViewAnimationOptionCurveEaseInOut animations:^{
+//        _password.center = CGPointMake(_password.center.x+self.view.frame.size.width, _password.center.y);
+//    } completion:nil];
+    
+//////////////////////////////////////////////第一章
+//    [UIView animateWithDuration:5 delay:0.5 usingSpringWithDamping:0.5 initialSpringVelocity:10.0 options:nil animations:^{
+//        self.loginButton.center = CGPointMake(self.loginButton.center.x, self.loginButton.center.y - 30.0);
+//        self.loginButton.alpha = 1.0;
+//    } completion:nil];
+
+    
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
